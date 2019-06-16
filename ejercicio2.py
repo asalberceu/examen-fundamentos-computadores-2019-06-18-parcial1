f0397de7ba981f7965c8fff4bf57c48ba409c9e3
Alfredo Sánchez Alberca <asalber@gmail.com>
2019-06-16 10:57:01 +0200

    Añadido ejercicio2.

---
+++ ejercicio2.py
@@ -0,0 +1,3 @@
+# Escribir un programa para ver si un alumno ha aprobado o no un curso con dos exámenes parciales.
+# Para aprobar el curso la nota media debe ser mayor o igual que 5 siempre y cuando en ambos parciales se tenga al menos un 4.
+
